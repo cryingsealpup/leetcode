40f52a1f5cce94fcbd459949ee2fecd697040125
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-05-19 22:38:18 +0400

    Time: 65 ms (22.90%) | Memory: 51.4 MB (77.14%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const def = init;
+    return {
+        increment: () => { init += 1; return init; },
+        decrement: () => { init -= 1; return init; },
+        reset: () => { init = def; return init; }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
