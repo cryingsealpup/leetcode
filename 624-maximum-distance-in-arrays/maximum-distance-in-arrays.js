c394f62b3d404109fce0818631541f0106f1c851
Ekaterina Radchenko <60271557+cryingsealpup@users.noreply.github.com>
2024-08-16 14:08:53 +0400

    Time: 109 ms (93.75%) | Memory: 74.3 MB (25.00%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.js
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.js
@@ -3,21 +3,20 @@
  * @return {number}
  */
 var maxDistance = function(arrays) {
-    let diff = 0;
     const maxs = [];
     const mins = [];
     arrays.forEach((el) => {
         maxs.push(el[el.length - 1]);
         mins.push(el[0]);
     });
-    let flag = true;
+
     let min = Math.min(...mins);
     let max = Math.max(...maxs);
-    diff = Math.abs(max - min);
+    let diff = Math.abs(max - min);
     if (maxs.indexOf(max) === mins.indexOf(min)) {
         maxs.splice(maxs.indexOf(max), 1);
         mins.splice(mins.indexOf(min), 1);
-        diff = Math.max(Math.abs(Math.max(...maxs) - min), Math.abs(max - Math.min(...mins)));
+        diff = Math.max(Math.max(...maxs) - min, max - Math.min(...mins));
     }
     return diff;
 };
